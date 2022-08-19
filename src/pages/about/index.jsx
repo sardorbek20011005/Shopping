63878229ab5039c827b6c3941dcf547a4edcca30
unknown <inomjonovsardorbek20011005@gmail.com>
2022-08-19 03:37:37 -0700

    about uploaded

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import Header from "../about/Header";
+import Body from "../about/Body";
+
+const IndexHome_1 = () => {
+	return (
+		<Box>
+			<Header />
+			<Body />
+		</Box>
+	);
+};
+
+export default IndexHome_1;
